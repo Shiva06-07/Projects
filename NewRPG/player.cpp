2927672860ef1b8992d8ceac7e300ebbde702185
SHIVAKUMAR <shivakumarbirapur@gmail.com>
2026-06-25 12:26:09 +0530

    Refactor buyItem to use unique_ptr for items

--- NewRPG/player.cpp
+++ NewRPG/player.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "player.h"
-
+#include <memory>
 Player::Player(const std::string &playerName, uint32_t playerGold)
 {
   name = playerName;
   gold = playerGold;
 }
-void Player::buyItem(const Item &itemToBuy)
+void Player::buyItem(std::unique_ptr<Item> itemToBuy)
 {
-  if (gold < itemToBuy.value)
+  if (gold < itemToBuy->getvalue())
   {
     std::cout << "Not enough gold to Buy :\n";
     return;
   }
-  gold -= itemToBuy.value;
-  Inventory.push_back(itemToBuy);
-  std::cout << "Item added to Inventory: " << itemToBuy.name << "\n";
+  gold -= itemToBuy->getvalue();
+  std::cout << "Item added to Inventory: " << itemToBuy->getname() << "\n";
+  Inventory.push_back(std::move(itemToBuy));
 }
