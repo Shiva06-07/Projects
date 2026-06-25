2044ad8d559dfe27f6bd2da2c68f5208649697ca
SHIVAKUMAR <shivakumarbirapur@gmail.com>
2026-06-25 12:23:40 +0530

    Refactor Item structure into classes with inheritance

--- NewRPG/item.h
+++ NewRPG/item.h
@@ -2,12 +2,56 @@
 
 #include <cstdint>
 #include <string>
+#include <iostream>
 
-struct Item
+class IEquipped
 {
-  std::string name;
-  double weight{0.0};
-  uint32_t id{0};
-  uint32_t value{0};
-  bool isEquipped{false};
+public:
+  virtual ~IEquipped() = default;
+  virtual void equip() const = 0;
 };
+class Item
+{
+protected:
+  std::string name;
+  uint32_t value{0};
+
+public:
+  Item(std::string wname, uint32_t wvalue) : name(wname), value(wvalue) {}
+  virtual ~Item() = default;
+  virtual void use() const
+  {
+    std::cout << "Used to print the usecase :\n";
+  }
+  [[nodiscard]] std::string getname() const { return name; }
+  [[nodiscard]] uint32_t getvalue() const { return value; }
+};
+
+class weapon : public Item, public IEquipped
+{
+  int damage;
+
+public:
+  weapon(std::string wname, uint32_t wvalue, int damage) : Item(wname, wvalue), damage(damage) {}
+  void use() const override
+  {
+    std::cout << ">>> You swing the " << name << " and deal " << damage << " DAMAGE!\n";
+  }
+  void equip() const override
+  {
+    std::cout << "[Equipped " << name << " to Main Hand]\n ";
+  }
+};
+
+class potion : public Item
+{
+  int healAmount;
+
+public:
+  potion(std::string n, uint32_t v, int healAmount) : Item(n, v), healAmount(healAmount) {}
+  void use() const override
+  {
+    std::cout << "+++ You drink the " << name << " and recover " << healAmount << " HP!\n";
+  }
+};
+
